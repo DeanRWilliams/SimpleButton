c888fe391a035f6565a11b9508fe034a4dd48d8d
Dean Williams <deanrwilliams@gmail.com>
2017-03-15 13:03:49 +0000

    Git familiarity

--- SimpleButton/ViewController.swift
+++ SimpleButton/ViewController.swift
@@ -10,17 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Comment 
+    // Main Label
     @IBOutlet weak var theLabel: UILabel!
     
+    @IBOutlet weak var rubyLabel: UILabel!
+    
+    var buttonpress = 0
     @IBAction func buttonChange(_ sender: Any) {
         theLabel.text = "Cool World!"
-        print("Cool Button tapped!")
+        rubyLabel.text = "Dean Label!"
+        buttonpress = buttonpress + 1
+        
+        if buttonpress > 10{
+            print("Button pressed more than 10 times!")}
     }
     
 
     @IBAction func buttonReser(_ sender: Any) {
         theLabel.text = "Hello World!"
+        rubyLabel.text = "Ruby Label!"
         print("Reset Button tapped")
     }
     override func viewDidLoad() {
